e579587c7d8574f659d404a789bde6f3a2911b35
FelipeVel <fvelandia695@gmail.com>
2025-05-28 20:26:03 -0500

    Añadir configuración de HttpClient y servicio de país; actualizar componente de búsqueda por capital

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { TableComponent } from "../../components/table/table.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   imports: [SearchInputComponent, TableComponent],
   templateUrl: './by-capital-page.component.html',
 })
 export class ByCapitalPageComponent {
+  private countryService = inject(CountryService);
   onSearchCapital(capital: string): void {
     console.log('Searching for capital:', capital);
     // Implement the search logic here
